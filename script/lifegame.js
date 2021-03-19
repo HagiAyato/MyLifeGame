3e8f6b321445e6d0e0d8bd2bc4be094b9bece6ce
HagiAyato <h8a9g11i.@gmail.com>
2021-03-19 18:25:22 +0900

    スマホのスワイプが切れる問題対応

--- script/lifegame.js
+++ script/lifegame.js
@@ -136,6 +136,9 @@ function OnMousemove(e) {
 
     if (debug_flg) document.getElementById('msg1').innerHTML = '現在座標　X:' + mouseX + ' Y' + mouseY;
     Point2BlockName(mouseX, mouseY);
+    // ドラッグ、スワイプによるこれ以上のイベントの発動を阻止
+    e.preventDefault();
+    e.stopPropagation();
 }
 
 function OnMouseup(e) {
